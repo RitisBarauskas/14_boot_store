c4bef49352231ccddb66f81afff9e483887922ba
Ritis Barauskas <perm300ru@yandex.ru>
2024-11-25 19:03:27 +0300

    <YANDEX-2> - upd model review

--- reviews/models.py
+++ reviews/models.py
@@ -1,5 +1,9 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+from django.db.models import ForeignKey
+
+YamDbUser = get_user_model()
 
 
 class Review(models.Model):
-    pass
+    author = ForeignKey(YamDbUser, on_delete=models.CASCADE, related_name='reviews')
